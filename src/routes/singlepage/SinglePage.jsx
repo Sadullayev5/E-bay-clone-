516746323b327b2f68e2d1db316fcb429f1ba56c
Sadullayev5 <168843403+Sadullayev5@users.noreply.github.com>
2024-08-23 05:06:34 +0500

    (

--- src/routes/singlepage/SinglePage.jsx
+++ src/routes/singlepage/SinglePage.jsx
@@ -3,7 +3,7 @@ import React, { useEffect , useState } from 'react'
 import { Link , useParams } from 'react-router-dom'
 import { useTranslation } from "react-i18next";
 import axios from "axios";
-import './SinglePage.css'
+import './singlepage.css'
 
 const SinglePage = () => {
 
